28abef4e78842f38bac69dc2a9015741dc71d85c
Abraham <abrahamon>
2016-03-21 15:02:49 -0600

    cascaron del servidor, contiene sockets con threads para recibir clientes, estos clientes se puede desconectar, apagar el servidor segun: "xexit" o "xkill" carga parametros del archivo configs.xml se escoge el puerto de conexcion del xml o itera hasta encontrar uno disponible

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # logorduino
+Curso: Lenguajes y compiladores, Marco Hernandez
+Creado por:
+-Abraham Arias
+-David Monestel
+-Fabian Solano
+-Lenin Torres
+
+-2016
---
+++ ServerApp/src/ServerInit.py
@@ -0,0 +1,112 @@
+import os
+import socket
+import time
+from xml.dom import minidom
+from threading import Thread
+
+
+
+'''--------------------------------------------------------------------------
+                    Variables globales
+ ----------------------------------------------------------------------------'''
+global DEBUG,PUERTO,ServerOn,Server
+
+
+
+
+'''--------------------------------------------------------------------------
+                    Configuracion de XML y Json
+ ----------------------------------------------------------------------------'''
+def loadXMLParameters():
+    global DEBUG,PUERTO,ServerOn
+
+    ruta=os.getcwd()
+    rutaFinal=str(ruta)+"/configs.xml"
+    xmlDocParametrosIni = minidom.parse(rutaFinal)
+    ServerOn=True
+
+    pDebug = xmlDocParametrosIni.getElementsByTagName('DEBUG')
+    load_DEBUG=pDebug[0].attributes['value'].value
+    if(load_DEBUG == "true"):
+        DEBUG = True
+    else:
+        DEBUG = False
+    pPuerto = xmlDocParametrosIni.getElementsByTagName('PUERTO')
+    PUERTO=int(pPuerto[0].attributes['value'].value)
+
+
+
+'''--------------------------------------------------------------------------
+                    Sockets concurrentes del servidor
+ ----------------------------------------------------------------------------'''
+def initServer():
+    global DEBUG,PUERTO,ServerOn,Server
+
+    Server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        if(DEBUG == True):
+            print".",
+        Server.bind(('', PUERTO))
+        Server.listen(5)
+        if(DEBUG == True):
+            print("\nServer initialized on the port: "+str(PUERTO))
+        ServerOn=True
+    except:
+        print".",
+        PUERTO+=1
+        initServer()
+
+
+
+
+def listen():
+    global DEBUG,ServerOn,Server
+
+    while ServerOn:
+        try:
+            conn, addr = Server.accept()
+            if(DEBUG):
+                print("**User: "+str(addr)+" Connected**")
+            b= Thread(target=handleClient, args=(conn,addr))
+            b.daemon = True
+            b.start()
+        except:
+            pass
+
+    Server.close()
+    if(DEBUG):
+        print("- Server turn off -")
+
+
+def handleClient(conn,addr):
+    global DEBUG, ServerOn
+
+    SessionOn= True
+    while SessionOn:
+        data = conn.recv(1024)
+        data = data.decode("utf-8")
+        data = data.split('\n')
+        try:
+            if(data[0] != ""):
+
+                if(data[0] == "xexit"):
+                    if(DEBUG== True):
+                        print("**usuario: "+str(addr)+" desconectado**")
+                    SessionOn=False
+                elif(data[0]=="xkill"):
+                    ServerOn=False
+                else:
+                    print("comando desconocido: "+data[0]+".   de :"+str(addr[0]))
+        except:
+            pass
+
+    conn.close()
+    #os._exit(0)
+
+
+
+
+loadXMLParameters()
+initServer()
+listen()
